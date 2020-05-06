846d498fa259515a038a4c74c7a91c62e2c0c33e
rinues850503@gmail.com <rinues850503@gmail.com>
2020-05-06 18:58:25 -0300

    Change in weather/routes/index.js => OWM_API_KEY

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const fetch = require("node-fetch");
 require('dotenv').config();
-const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
+const OWM_API_KEY = 'd68b5e047adb79257e2d682050f1c958';
+//const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
 const UNITS = process.env.UNITS || 'metric';
 
 /* GET home page. */
